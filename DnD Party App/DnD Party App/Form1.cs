48811735418be4cb362bad9a8949ddb8575690a0
Dylan <dhollan7@kent.edu>
2018-01-28 17:06:47 -0500

    Tirmed down the code

--- DnD Party App/DnD Party App/Form1.cs
+++ DnD Party App/DnD Party App/Form1.cs
@@ -18,18 +18,11 @@ namespace DnD_Party_App
             InitializeComponent();
         }
 
-
-
-        private async Task testAsync()
+        private async void button1_ClickAsync(object sender, EventArgs e)
         {
             HttpClient client = new System.Net.Http.HttpClient();
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "kAvJer4b6Hd4yWWlrAfHmrTsnQtJXAk75PthaNoxUcWtRIIRIBTM5YNU_maCnLOTusfiKG-6jZCTfWHqA9QJAsNgSaO7txjb7T9-lJJ22ZEj84s_0dccmy38AEppWnYx" );
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "kAvJer4b6Hd4yWWlrAfHmrTsnQtJXAk75PthaNoxUcWtRIIRIBTM5YNU_maCnLOTusfiKG-6jZCTfWHqA9QJAsNgSaO7txjb7T9-lJJ22ZEj84s_0dccmy38AEppWnYx");
             richTextBox1.Text = await (await client.GetAsync("https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972")).Content.ReadAsStringAsync();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            testAsync();
         }
     }
 }
