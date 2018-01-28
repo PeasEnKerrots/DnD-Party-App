9bd6c3205dfc499e6eeae5467a934d6d6f4eba68
Dylan <dhollan7@kent.edu>
2018-01-27 20:39:48 -0500

    Testing the get. Onto google...

--- DnD Party App/DnD Party App/Form1.cs
+++ DnD Party App/DnD Party App/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,21 @@ namespace DnD_Party_App
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        private async Task testAsync()
+        {
+            HttpClient client = new System.Net.Http.HttpClient();
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("kAvJer4b6Hd4yWWlrAfHmrTsnQtJXAk75PthaNoxUcWtRIIRIBTM5YNU_maCnLOTusfiKG-6jZCTfWHqA9QJAsNgSaO7txjb7T9-lJJ22ZEj84s_0dccmy38AEppWnYx");
+            HttpResponseMessage result = await client.GetAsync("https://api.yelp.com/v3/businesses/search/+12162263699");
+            string tmp = await result.Content.ReadAsStringAsync();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            testAsync();
         }
     }
 }
