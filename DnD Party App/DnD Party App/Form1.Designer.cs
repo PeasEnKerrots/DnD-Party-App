e031762029131b8c5b1d1023b929b628534f1100
Dylan <dhollan7@kent.edu>
2018-05-14 20:13:06 -0400

    nZSJcxdvfks

--- DnD Party App/DnD Party App/Form1.Designer.cs
+++ DnD Party App/DnD Party App/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -50,16 +52,36 @@
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(156, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(867, 20);
+            this.textBox1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(891, 463);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +89,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- DnD Party App/DnD Party App/Form1.cs
+++ DnD Party App/DnD Party App/Form1.cs
@@ -20,9 +20,20 @@ namespace DnD_Party_App
 
         private async void button1_ClickAsync(object sender, EventArgs e)
         {
-            HttpClient client = new System.Net.Http.HttpClient();
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "kAvJer4b6Hd4yWWlrAfHmrTsnQtJXAk75PthaNoxUcWtRIIRIBTM5YNU_maCnLOTusfiKG-6jZCTfWHqA9QJAsNgSaO7txjb7T9-lJJ22ZEj84s_0dccmy38AEppWnYx");
-            richTextBox1.Text = await (await client.GetAsync("https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972")).Content.ReadAsStringAsync();
+            using (HttpClient client = new System.Net.Http.HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "kAvJer4b6Hd4yWWlrAfHmrTsnQtJXAk75PthaNoxUcWtRIIRIBTM5YNU_maCnLOTusfiKG-6jZCTfWHqA9QJAsNgSaO7txjb7T9-lJJ22ZEj84s_0dccmy38AEppWnYx");
+                richTextBox1.Text = await (await client.GetAsync("https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972")).Content.ReadAsStringAsync();
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new System.Net.Http.HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "kAvJer4b6Hd4yWWlrAfHmrTsnQtJXAk75PthaNoxUcWtRIIRIBTM5YNU_maCnLOTusfiKG-6jZCTfWHqA9QJAsNgSaO7txjb7T9-lJJ22ZEj84s_0dccmy38AEppWnYx");
+                richTextBox1.Text = await (await client.GetAsync(string.Format("https://api.yelp.com/v3/businesses/search?term={0}&latitude=41.4993&longitude=-81.6944", textBox1.Text))).Content.ReadAsStringAsync();                
+            }
         }
     }
 }
